747a4c0dd98dc4eb11f18b07cce1ad28cb12e162
Rajasekar T <kt.rajasekar@gmail.com>
2019-10-22 08:34:11 +0530

    js added

---
+++ javascript/js/custom.js
@@ -0,0 +1,20 @@
+console.log("INSIDE --------------------------");
+
+// Variables 
+
+// Global 
+
+
+function a(){
+    var testvalue = 45;
+    alert(testvalue)
+}
+
+function b(){
+    console.log(testvalue);
+    alert(testvalue)
+}
+
+a();
+b();
+
